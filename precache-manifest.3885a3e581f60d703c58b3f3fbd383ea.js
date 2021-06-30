d5e4383948b9786abba0a0b82e9719ab64d671d3
Andre Alexander Bell <github@andre-bell.de>
2021-06-30 17:38:00 +0000

    Updates

---
+++ precache-manifest.3885a3e581f60d703c58b3f3fbd383ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/template_elm-app/favicon.ico"
+  },
+  {
+    "revision": "3a06a1983ad7851412063d94957fefb9",
+    "url": "/template_elm-app/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/template_elm-app/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/template_elm-app/manifest.json"
+  },
+  {
+    "revision": "f912a49f25fb3e9645df",
+    "url": "/template_elm-app/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "f912a49f25fb3e9645df",
+    "url": "/template_elm-app/static/js/main.f912a49f.chunk.js"
+  },
+  {
+    "revision": "2094e6a45378a5e1b921",
+    "url": "/template_elm-app/static/js/runtime~main.2094e6a4.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/template_elm-app/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
